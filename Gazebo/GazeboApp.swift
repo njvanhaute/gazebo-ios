4bb8e018095fe30a07465dfb7cab6fcebca37f16
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-08-08 22:57:41 -0500

    Initial Commit

---
+++ Gazebo/GazeboApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazeboApp.swift
+//  Gazebo
+//
+//  Created by Nicholas Vanhaute on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GazeboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
